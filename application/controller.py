deb67c6370f7d2b8382df5fcd79ea6ae24faf6c8
Vimal <kvimal13@gmail.com>
2016-01-14 14:28:46 +0530

    changed host

--- application/controller.py
+++ application/controller.py
@@ -46,7 +46,7 @@ def start():
 
 
 if __name__ == '__main__':
-	cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', 5000))})
+	cherrypy.config.update({'server.socket_host': '0.0.0.0','server.socket_port': int(os.environ.get('PORT', 5000))})
 	start()
 
 
